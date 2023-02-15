91a015eefe8180faf611bdeecd68dc1b4643fc8e
Sango <analysteprodez.com>
2023-02-15 14:59:44 +0000

    week7 day4 xp

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import exercice1.Cercle;
+import exercice2.Calcul;
+import exercice3.GenerateurNombre;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Le perimetre: " + Cercle.perimetre(2.6));
+        System.out.println("L'aire est " + Cercle.area(2.6));
+
+        //Exercice 2
+        Calcul.execute();
+
+        //Exercice 3
+        GenerateurNombre.execute();
+    }
+}
